34c91be7d21351736bfad1cc2af3d2dc170c44f2
Zeb Rasco <31866081+zrasco@users.noreply.github.com>
2020-09-15 18:04:35 -0700

    Add project files.

---
+++ ExampleDaily/Program.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.*")]
+
+namespace ExampleDaily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get our assembly version in the form of (Major.Minor.Daily.Revision)
+            string version = Assembly.GetEntryAssembly().GetName().Version.ToString();
+
+            // Set up Serilog to log to console
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            // Log.<LogLevel> will now print to the console
+            Log.Information($"This is our example daily build program! Current version: {version}");
+        }
+    }
+}
